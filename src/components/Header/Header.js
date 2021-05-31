154794b7ee824681c01bee1260b9a1a7044ecaca
wpMcDonnell <will.mcdonnell4@gmail.com>
2021-05-30 22:39:22 -0400

    Add ? in homepage for query field

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import Navbar from 'react-bootstrap/Navbar'
 
 const options = (
   <Fragment>
-    <Nav.Link href="#/">About</Nav.Link>
+    <Nav.Link href="?#/">About</Nav.Link>
     <Nav.Link href="#search">Search</Nav.Link>
     <Nav.Link href="#history">History</Nav.Link>
   </Fragment>
